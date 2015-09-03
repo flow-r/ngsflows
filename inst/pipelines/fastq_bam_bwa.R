e69c0e0f2e460c931e2dfc19f4d4fb37782dd86f
sseth <me@sahilseth.com>
2015-09-03 14:25:29 -0500

    default naming of flow mat and using check_args, for more relaiability

--- inst/pipelines/fastq_bam_bwa.R
+++ inst/pipelines/fastq_bam_bwa.R
@@ -30,51 +30,54 @@ fastq_bam_bwa <- function(
   fqs1, fqs2,
   outfile,
   samplename = get_opts("samplename")){
-
-    pipename = match.call()[[1]]
-    message("Generating a ", pipename, " flowmat for sample: ", samplename)
-
-    ## --- all subsequent steps would use this samplename
-    set_opts(samplename = samplename)
-
-    ## Calling modules, each returns
-    ##   - a vector of outfiles
-    ##   - a flowmat, which we need to rbind and are done !
-    out_bwa = bwa(fqs1 = fqs1, fqs2 = fqs2)
-    out_rg = picard_rg(out_bwa$outfiles)
-    if(missing(outfile))
-      outfile = sprintf("%s.rg.sorted.bam", samplename) ## feel free to change this !
-    out_merge = picard_merge(out_rg$outfiles, mergedbam = outfile)
-
-    ##--- merging three flowmats
-    flowmat = rbind(out_bwa$flowmat, out_rg$flowmat, out_merge$flowmat)
-
-    return(list(outfile=outfile, flowmat = flowmat))
-  }
+  
+  
+  ## --- all subsequent steps would use this samplename
+  
+  check_args()
+  set_opts(samplename = samplename)
+  pipename = match.call()[[1]]
+  message("Generating a ", pipename, " flowmat for sample: ", samplename)
+  
+  
+  ## Calling modules, each returns
+  ##   - a vector of outfiles
+  ##   - a flowmat, which we need to rbind and are done !
+  out_bwa = bwa(fqs1 = fqs1, fqs2 = fqs2)
+  out_rg = picard_rg(out_bwa$outfiles)
+  if(missing(outfile))
+    outfile = sprintf("%s.rg.sorted.bam", samplename) ## feel free to change this !
+  out_merge = picard_merge(out_rg$outfiles, mergedbam = outfile)
+  
+  ##--- merging three flowmats
+  flowmat = rbind(out_bwa$flowmat, out_rg$flowmat, out_merge$flowmat)
+  
+  return(list(outfile=outfile, flowmat = flowmat))
+}
 
 
 if(FALSE){
-
+  
   require(flowr)
   load_opts(search_conf("ngsflows_mda.conf"))
-
+  
   ## This fails, extension seems weird
   flow_mat = aln_bwa_merge(fqs1 = rep("hello.fq.gz", 10),
-    fqs2 = rep("hello.fq", 10),
-    samplename = "smp")
-
-
+                           fqs2 = rep("hello.fq", 10),
+                           samplename = "smp")
+  
+  
   ## This fails, length is not the same, for paired end
   flow_mat = aln_bwa_merge(fqs1 = rep("hello.fq", 10),
-    fqs2 = rep("hello.fq", 11),
-    samplename = "smp")
-
-
+                           fqs2 = rep("hello.fq", 11),
+                           samplename = "smp")
+  
+  
   out_alnmerge = aln_bwa_merge(fqs1 = rep("hello.fq", 10),
-    fqs2 = rep("hello.fq", 10),
-    samplename = "smp")
-
-
+                               fqs2 = rep("hello.fq", 10),
+                               samplename = "smp")
+  
+  
 }
 
 
@@ -83,11 +86,11 @@ main <- function(){
     fqs1 = rep("hello.fq", 10),
     fqs2 = rep("hello.fq", 10),
     samplename = "smp")
-
+  
   flowmat =  out_alnmerge$flowmat
   def = to_flowdef(flowmat)
   plot_flow(to_flow(flowmat, def))
-
+  
 }
 
 
