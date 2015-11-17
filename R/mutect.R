d4d5a801d6b28e65e79423fcec903890ac395486
sseth <me@sahilseth.com>
2015-11-17 13:52:57 -0600

    merge_sheets now converts everything to character.

--- R/mutect.R
+++ R/mutect.R
@@ -107,6 +107,9 @@ merge_sheets <- function(x, outfile, .filter = NA, ...){
   tmp <- lapply(x, function(fl){
     message(".", appendLF = FALSE)
     tab = read_sheet(fl, ...)
+    # convert all columns to character, fool proofing
+    tab[] <- lapply(x, as.character)
+
     if(!is.na(.filter)){
       tab2 = dplyr::filter_(tab, .filter)
       return(tab2)
@@ -115,7 +118,10 @@ merge_sheets <- function(x, outfile, .filter = NA, ...){
     }
   })
   
-  mrgd = do.call(rbind, tmp)
+  #mrgd = try(do.call(rbind, tmp))
+  # if fails try using dplyr
+  mrgd = try(bind_rows(tmp))
+  
   if(!missing(outfile))
     write_sheet(mrgd, outfile)
   
